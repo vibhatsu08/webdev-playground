4910334895ea34091770cbe3413abe3594fd6059
Vedant Mistry <mistryvedant17@gmail.com>
2022-08-13 02:28:01 +0530

    added documentation

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// created a variable for finding the element with the container id.
 const container = document.querySelector('#container');
 
+// created the grid variable for housing the cells for the array of boxes.
 let grid = document.createElement('div');
 grid.classList.add('grid');
 
+// user entered number to create a box with equal number of rows and columns.
 let number = window.prompt("enter a number: ");
+// copied the value of the number in a new variable, to take it as a reference in the later part of the code.
 let gridNumber = number;
 
+// function to generate the grid based on the user input number.
 function generateGrid (number) {
     for (let i=1; i<=number*number; i++) {
         let cell = document.createElement('div');
@@ -15,10 +20,11 @@ function generateGrid (number) {
     container.appendChild(grid);
 }
 
-
+// this function acts when the window is loaded.
 window.addEventListener (
     'load', () => {
         generateGrid(number);
+        // sets the required property for the grid.
         grid.style.setProperty('--gridNumber', gridNumber);
     }
 )
