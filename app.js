1d070dd5a19be05052bb55936c61e217a1a9df0c
Vedant Mistry <mistryvedant17@gmail.com>
2022-08-14 02:45:58 +0530

    Etch a Sketch Project

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 // gave the page container a variable.
 const container = document.querySelector('#container');
+// referring to the buttons-and-grid div
+const buttonsAndGrid = document.querySelector('#buttons-and-grid');
 // created a grid variable to hold the grid cells in the webpage, and adding the class grid to the declared variable.
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
 // window prompt will ask the user for the number of boxes to be displayed.
-let number = window.prompt("Enter a random number");
+let number = 16
 let gridNumber = number;
 
+// variable to store the user input value
+const userInput = document.querySelector('#quantity');
+
+// variable to call the reset button
+const resetButton = document.querySelector('#reset-grid-button');
+
 // function to create the grid
-function generateGrid (number) {
-// for loop to display an equal number of boxes, based on the user provided number.
+generateGrid = (number) => {
+    // for loop to display an equal number of boxes, based on the user provided number.
     for (let i=1; i<=number*number; i++) {
         let box = document.createElement('div');
         box.classList.add('box');
         grid.appendChild(box);
     }
-    container.appendChild(grid);
+    buttonsAndGrid.appendChild(grid);
+    container.appendChild(buttonsAndGrid);
+    // sets the required property for the grid.
+    grid.style.setProperty('--gridNumber', gridNumber);
 }
 
 // event listener to change the color of the box when hover
@@ -28,20 +39,38 @@ grid.addEventListener (
     }
 );
 
-// function to display the grid when the window is loaded.
-window.addEventListener (
-    'load', () => {
-        generateGrid(number);
-        // sets the required property for the grid.
-        grid.style.setProperty('--gridNumber', gridNumber);
+// function to update the grid
+updateGrid = () => {
+    grid.innerHTML = "";
+    grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 1fr)`);
+
+    for (let i=1; i<=userInput.value*userInput.value; i++) {
+        let box = document.createElement('div');
+        box.classList.add('box');
+        grid.appendChild(box);
+    }
+    buttonsAndGrid.appendChild(grid);
+    container.appendChild(buttonsAndGrid);
+    grid.style.setProperty('--gridNumber', '1fr');
+}
+
+userInput.addEventListener (
+    "change", updateGrid
+);
+
+// function to reset the grid, removes the background color classes from the box after the mousedown event occurs.
+resetButton.addEventListener (
+    'click', () => {
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+        }
+        generateGrid(userInput.value);
     }
 )
 
-// function to reset the grid, removes the background color classes from the box after the mousedown event occurs.
-function resetGrid () {
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
-    }
-    generateGrid(number);
-}
+// calling the function to display the grid
+generateGrid();
+
+
 
