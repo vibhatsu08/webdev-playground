b268c8a58e3aabfe557a50f224ddacc2edfc70ab
vibhatsu08 <mistryvedant17@gmail.com>
2022-10-07 01:11:26 +0530

    Function for the modal, when the button is clicked, to open and when the button inside the form is clicked, to close the modal.

---
+++ app.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById('instructions-form-popup').style.display = 'block';  
+}
+function closeForm() {
+    document.getElementById('instructions-form-popup').style.display = 'none';
+}
