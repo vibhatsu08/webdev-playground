5ea80286fcf4491e7c36d47b35b3bc269f8e12e1
vibhatsu08 <mistryvedant17@gmail.com>
2022-10-09 23:24:48 +0530

    Wrote the basic script for opening the modals linked to the buttons in the DOM.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function openForm() {
-    document.getElementById('instructions-form-popup').style.display = 'block';  
-}
-function closeForm() {
-    document.getElementById('instructions-form-popup').style.display = 'none';
+let modalButtons = [...document.querySelectorAll(".modal-buttons")];
+modalButtons.forEach (
+    function (button) {
+        button.onclick = function () {
+            let modal = button.getAttribute("data-model");
+            document.getElementById(modal).style.display = "block";
+        }
+    }
+);
+window.onclick = function (event) {
+    if (event.target.className === "modal") {
+      event.target.style.display = "none";
+    }
 }
