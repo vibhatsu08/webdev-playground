ac0604b8b56c13161fde62e5cd98d83a678ec2f7
vibhatsu08 <mistryvedant17@gmail.com>
2022-10-15 01:23:00 +0530

    Wrote the script for adding the note groups into the DOM, and also the delete groups now work as intended, when clicked the delete group buttons delete the child closes to the button, which is the parent of that particular child button.

--- app.js
+++ app.js
@@ -1,4 +1,59 @@
-// for opening and closing the modals
+const newNoteGroupModalInput = document.getElementById("new-type-modal-input");
+const newNoteModalInput = document.getElementById("new-note-modal-input");
+
+let noteGroups = ["programming", "shopping", "books"];
+const notesBody = document.getElementById("notes-body");
+
+// for adding the note groups into the DOM.
+createNoteGroup = (noteGroups) => {
+    let notesGroupFragment = document.createDocumentFragment();
+
+    noteGroups.forEach (
+        function (noteGroup) {
+            let notesDiv = document.createElement("div");
+            let notesHeaderDiv = document.createElement("div");
+            let notesHeader = document.createElement("div");
+            let notesHeaderButtons = document.createElement("div");
+            
+            let deleteGroupButton = document.createElement("button");
+            let newNoteButton = document.createElement("button");
+
+            notesDiv.classList.add("notes-div");
+            notesHeaderDiv.classList.add("notes-header-div");
+            notesHeader.classList.add("notes-header");
+            notesHeaderButtons.classList.add("notes-header-buttons");
+            deleteGroupButton.classList.add("delete-group-button");
+            newNoteButton.classList.add("new-note-button");
+            newNoteButton.classList.add("modal-buttons");
+
+            notesHeader.textContent = noteGroup;
+            deleteGroupButton.textContent = "delete group";
+            newNoteButton.textContent = "new note";
+
+            notesHeaderDiv.appendChild(notesHeader);
+            notesHeaderButtons.appendChild(deleteGroupButton);
+            notesHeaderButtons.appendChild(newNoteButton);
+            notesHeaderDiv.appendChild(notesHeaderButtons);
+            notesDiv.appendChild(notesHeaderDiv);
+            notesGroupFragment.appendChild(notesDiv);
+        });
+    notesBody.appendChild(notesGroupFragment);
+}
+createNoteGroup(noteGroups);
+
+// for deleting the particular group when the delete group button is clicked.
+let deleteGroupButtons = [...document.querySelectorAll(".delete-group-button")];
+deleteGroupButtons.forEach (
+    function (button) {
+        button.onclick = function () {
+            let group = button.closest(".notes-div");
+            notesBody.removeChild(group);
+        }
+    }
+);
+
+
+// for opening the modals
 let openModalButtons = [...document.querySelectorAll(".modal-buttons")];
 openModalButtons.forEach (
     function (button) {
@@ -8,7 +63,8 @@ openModalButtons.forEach (
         }
     }
 );
-let closeModalButtons = [...document.querySelectorAll(".modal-close-button")]
+// for closing the modals
+let closeModalButtons = [...document.querySelectorAll(".modal-close-button")];
 closeModalButtons.forEach (
     function (button) {
         button.onclick = function () {
@@ -16,10 +72,4 @@ closeModalButtons.forEach (
             modal.style.display = "none";
         }
     }
-)
-
-window.onclick = function (event) {
-    if (event.target.className === "modal") {
-      event.target.style.display = "none";
-    }
-}
+);
