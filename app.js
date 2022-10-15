133b361826f46110fb38e5c24e1fad2c6be839f7
vibhatsu08 <mistryvedant17@gmail.com>
2022-10-15 16:42:26 +0530

    Fixed the bug for the new note group button, now instead of storing the note group headers in an array, they are just created into their respective DOM elements.

--- app.js
+++ app.js
@@ -1,57 +1,98 @@
 const newNoteGroupModalInput = document.getElementById("new-type-modal-input");
 const newNoteModalInput = document.getElementById("new-note-modal-input");
 
-let noteGroups = ["programming", "shopping", "books"];
+const newNoteGroupModalDoneButton = document.getElementById("type-done-button");
+const newNoteModalDoneButton = document.getElementById("note-done-button");
+
+// const deleteGroupButton = document.querySelectorAll(".delete-group-button");
+
+
 const notesBody = document.getElementById("notes-body");
 
-// for adding the note groups into the DOM.
-createNoteGroup = (noteGroups) => {
-    let notesGroupFragment = document.createDocumentFragment();
 
-    noteGroups.forEach (
-        function (noteGroup) {
-            let notesDiv = document.createElement("div");
-            let notesHeaderDiv = document.createElement("div");
-            let notesHeader = document.createElement("div");
-            let notesHeaderButtons = document.createElement("div");
-            
-            let deleteGroupButton = document.createElement("button");
-            let newNoteButton = document.createElement("button");
-
-            notesDiv.classList.add("notes-div");
-            notesHeaderDiv.classList.add("notes-header-div");
-            notesHeader.classList.add("notes-header");
-            notesHeaderButtons.classList.add("notes-header-buttons");
-            deleteGroupButton.classList.add("delete-group-button");
-            newNoteButton.classList.add("new-note-button");
-            newNoteButton.classList.add("modal-buttons");
-
-            notesHeader.textContent = noteGroup;
-            deleteGroupButton.textContent = "delete group";
-            newNoteButton.textContent = "new note";
-
-            notesHeaderDiv.appendChild(notesHeader);
-            notesHeaderButtons.appendChild(deleteGroupButton);
-            notesHeaderButtons.appendChild(newNoteButton);
-            notesHeaderDiv.appendChild(notesHeaderButtons);
-            notesDiv.appendChild(notesHeaderDiv);
-            notesGroupFragment.appendChild(notesDiv);
-        });
-    notesBody.appendChild(notesGroupFragment);
-}
-createNoteGroup(noteGroups);
-
-// for deleting the particular group when the delete group button is clicked.
-let deleteGroupButtons = [...document.querySelectorAll(".delete-group-button")];
-deleteGroupButtons.forEach (
-    function (button) {
-        button.onclick = function () {
-            let group = button.closest(".notes-div");
-            notesBody.removeChild(group);
-        }
+newNoteGroupModalDoneButton.addEventListener (
+    "click", function () {
+        console.log(newNoteGroupModalInput.value);
+        createNoteGroupSingle(newNoteGroupModalInput.value);
     }
 );
+newNoteModalDoneButton.addEventListener (
+    "click", function () {
+        console.log(newNoteModalInput.value);
+        createNoteSingle(newNoteModalInput.value);
+    }
+); 
 
+createNoteGroupSingle = (noteGroup) => {
+    let noteGroupFragment = document.createDocumentFragment();
+        
+    let notesDiv = document.createElement("div");
+    let notesHeaderDiv = document.createElement("div");
+    let notesHeader = document.createElement("div");
+    let notesHeaderButtons = document.createElement("div");
+            
+    let deleteGroupButton = document.createElement("button");
+    let newNoteButton = document.createElement("button");
+
+    notesDiv.classList.add("notes-div");
+    notesHeaderDiv.classList.add("notes-header-div");
+    notesHeader.classList.add("notes-header");
+    notesHeaderButtons.classList.add("notes-header-buttons");
+    deleteGroupButton.classList.add("delete-group-button");
+    newNoteButton.classList.add("new-note-button");
+    newNoteButton.classList.add("modal-buttons");
+
+    notesHeader.textContent = noteGroup;
+    deleteGroupButton.textContent = "delete group";
+    newNoteButton.textContent = "new note";
+    newNoteButton.setAttribute("data-model", "new-note-modal");
+
+    notesHeaderDiv.appendChild(notesHeader);
+    notesHeaderButtons.appendChild(deleteGroupButton);
+    notesHeaderButtons.appendChild(newNoteButton);
+    notesHeaderDiv.appendChild(notesHeaderButtons);
+    notesDiv.appendChild(notesHeaderDiv);
+    noteGroupFragment.appendChild(notesDiv);
+    notesBody.appendChild(notesDiv);
+
+    return notesDiv;
+}
+
+createNoteSingle = (noteValue) => {
+    let noteFragment = document.createDocumentFragment();
+
+    let notes = document.createElement("div");
+    let note = document.createElement("div");
+    let noteText = document.createElement("div");
+    let noteButtons = document.createElement("div");
+
+    let noteEditButton = document.createElement("button");
+    let noteDoneButton = document.createElement("button");
+    let noteDeleteButton = document.createElement("button");
+
+    notes.classList.add("notes");
+    note.classList.add("note");
+    noteText.classList.add("note-text");
+    noteButtons.classList.add("note-buttons");
+    noteEditButton.classList.add("note-edit-button");
+    noteDoneButton.classList.add("note-done-button");
+    noteDeleteButton.classList.add("note-delete-button");
+
+    noteText.textContent = noteValue;
+    noteEditButton.textContent = "edit";
+    noteDoneButton.textContent = "done";
+    noteDeleteButton.textContent = "delete";
+
+    note.appendChild(noteText);
+    noteButtons.appendChild(noteEditButton);
+    noteButtons.appendChild(noteDoneButton);
+    noteButtons.appendChild(noteDeleteButton);
+    notes.appendChild(note);
+    notes.appendChild(noteButtons);
+    notesDiv.appendChild(notes);
+    noteFragment.appendChild(notesDiv);
+    notesBody.appendChild(noteFragment);
+}
 
 // for opening the modals
 let openModalButtons = [...document.querySelectorAll(".modal-buttons")];
